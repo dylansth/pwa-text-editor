b468fb335769fa1700d88e0f56f22b064e2e48c1
Simba <dylan.sthilaire@proton.me>
2023-07-09 16:02:03 -0700

    debugging

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,22 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Asset caching
+registerRoute(
+  ({ request }) => request.destination === 'script' ||
+    request.destination === 'style',
+    // CacheFirst: Retrieve from cache or fallback to network 
+  new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      // Cache response status codes 0, 200
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60, // cache expiration
+      }),
+    ],
+  })
+);
+
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,30 @@ const initdb = async () =>
     },
   });
 
+  // initdb().then((db) => {
+  //   console.log('Database instance:', db);
+  // });
+
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // console.error('putDb not implemented');
+  // const db = await initdb();
+  // const tx = db.transaction('jate', 'readwrite');
+  // const store = tx.objectStore('jate');
+  // await store.add(content);
+  // tx.oncomplete;
+  // console.log('putDb Successful.')
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // console.error('getDb not implemented');
+  // const db = await initdb();
+  // const tx = db.transaction('jate', 'readonly');
+  // const store = tx.objectStore('jate');
+  // const content = await store.getAll();
+  // tx.oncomplete;
+  // return content;
+}
 
 initdb();
